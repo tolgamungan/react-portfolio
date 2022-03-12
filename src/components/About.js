322a771b4b751f54eb0c22c7cc8208d7b507a5dd
Tolga Mungan <tmungann@gmail.com>
2022-03-12 02:08:40 -0400

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="about">
+         <div className="row">
+
+            <div className="three columns">
+
+               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+
+            </div>
+
+            <div className="nine columns main-col">
+
+               <h2>About Me</h2>
+               <p>
+               {
+                 resumeData.aboutme
+               }
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       						<span>{resumeData.name} {resumeData.lastname}</span>
+                     <br></br>
+       						   <span>
+                     {resumeData.address}
+                    </span>
+                    <br></br>
+                    <span>{resumeData.email}</span>
+       					   </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,121 @@
+let resumeData = {
+    "imagebaseurl":"tolgamungan.com",
+    "name": "Tolga",
+    "lastname": "Mungan",
+    "role": "Full-Stack Web Developer",
+    "linkedinId":"tmungan",
+    "skypeid": "skypeid",
+    "roleDescription": "👨🏻‍💻 located in Sydney, NS.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/tmungan/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/tolgamungan",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/tmungann",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a final year student at Nova Scotia Community College in IT Web Programming program.",
+    "address":"Sydney, NS",
+    "website":"https://tolgamungan.com/",
+    "email":"tmungann@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Nova Scotia Community College",
+        "specialization":"Diploma, IT Web Programming",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Express.js"
+      },
+      {
+        "skillname":"React.js"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"Typescript"
+      },
+      {
+        "skillname":"RESTful API"
+      },
+      {
+        "skillname":"ASP.NET Core"
+      },
+      {
+        "skillname":"C#"
+      },
+      {
+        "skillname":"EF Core"
+      },{
+        "skillname":"Docker"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
