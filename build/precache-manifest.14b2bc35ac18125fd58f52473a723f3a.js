b9250709c23a51095d4f6125aca113fc68f517c5
Tolga Mungan <tmungann@gmail.com>
2022-03-18 03:58:49 -0300

    commit

---
+++ build/precache-manifest.14b2bc35ac18125fd58f52473a723f3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6693691a5b8e42e44c0",
+    "url": "/react-portfolio/static/css/main.2dcf629b.chunk.css"
+  },
+  {
+    "revision": "a6693691a5b8e42e44c0",
+    "url": "/react-portfolio/static/js/main.a6693691.chunk.js"
+  },
+  {
+    "revision": "c075425bab91b4fc39ea",
+    "url": "/react-portfolio/static/js/1.c075425b.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "1207a79d3e4c464b5c534ccf7553e2f7",
+    "url": "/react-portfolio/index.html"
+  }
+];
