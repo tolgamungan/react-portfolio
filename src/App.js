3774c222381dc15058a22fce9a778dde19d8932c
Tolga Mungan <tmungann@gmail.com>
2023-03-27 07:23:55 -0300

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
-import Testimonials from  './components/Testimonials';
-import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 import Certifications from './components/Certifications';
@@ -13,26 +11,17 @@ import Certifications from './components/Certifications';
 class App extends Component {
   render() {
     return ( 
-
-      <Route
-          // path="/"
-          render={() => 
-          <div className="App">
-            <Header resumeData={resumeData}/>
-            <About resumeData={resumeData}/>
-            <Resume resumeData={resumeData}/>
-            <Certifications resumeData={resumeData} />
-            <Portfolio resumeData={resumeData}/>
-            <Footer resumeData={resumeData}/>
-          </div>
-        }
-        // exact 
-        />
-        
-        // <Testimonials resumeData={resumeData}/>
-      //   {/* <ContactUs resumeData={resumeData}/> */}
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <Routes>
+          <Route path="/" element={<About resumeData={resumeData} />} />
+          <Route path="/resume" element={<Resume resumeData={resumeData} />} />
+          <Route path="/certifications" element={<Certifications resumeData={resumeData} />} />
+          <Route path="/portfolio" element={<Portfolio resumeData={resumeData} />} />
+        </Routes>
+        <Footer resumeData={resumeData}/>
+      </div>
     );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
+// import BrowserRouter from 'react-router-dom/BrowserRouter';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
       <BrowserRouter>
